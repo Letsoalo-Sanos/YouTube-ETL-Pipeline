2996111df74981751394874ff8df488d1212d6dd
Sanos <letsoalo.sanos@gmail.com>
2026-06-21 13:36:03 +0200

    Add functions for schema and table creation in data_utils.py

--- dags/datawarehouse/data_utils.py
+++ dags/datawarehouse/data_utils.py
@@ -1,6 +1,8 @@
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from pyscopg2.extras import RealDictCursor
 
+table = "yt_api"
+
 def get_conn_cursor():
   hook = PostgresHook(postgres_conn_id="postgres_db_yt_elt",database="elt_db")
   conn = hook.get_conn()
@@ -10,3 +12,59 @@ def get_conn_cursor():
 def close_conn_cursor(conn, cursor):
   cursor.close()
   conn.close()
+
+def create_schema(schema):
+
+  conn, cursor = get_conn_cursor()
+
+  schema_sql = f"CREATE SCHEMA IF NOT EXISTS {schema};"
+  cursor.execute(schema_sql)
+  conn.commit()
+
+  close_conn_cursor(conn, cursor)
+
+def create_table(schema):
+
+  conn, cursor = get_conn_cursor()
+
+  if schema == "staging":
+    table_sql = f"""
+    CREATE TABLE IF NOT EXISTS {schema}.{table} (
+      video_id VARCHAR(20) PRIMARY KEY,
+      "Video_Title" TEXT NOT NULL,
+      "Upload_Date" TIMESTAMP NOT NULL,
+      "Duration" VARCHAR(20) NOT NULL,
+      "Video_Views" INTEGER,
+      "Likes_Count" INTEGER,
+      "Comments_Count" INTEGER
+    );
+
+    """
+
+  else:
+     table_sql = f"""
+    CREATE TABLE IF NOT EXISTS {schema}.{table} (
+      video_id VARCHAR(20) PRIMARY KEY,
+      "Video_Title" TEXT NOT NULL,
+      "Upload_Date" TIMESTAMP NOT NULL,
+      "Duration" VARCHAR(20) NOT NULL,
+      "Video_Views" INTEGER,
+      "Likes_Count" INTEGER,
+      "Comments_Count" INTEGER
+    );
+
+    """
+     
+  cursor.execute(table_sql)
+  conn.commit()
+
+  close_conn_cursor(conn, cursor)
+
+def get_video_ids(cursor,schema):
+
+  cursor.execute(f"SELECT video_id FROM {schema}.{table};")
+  ids = cursor.fetchall()
+
+  video_ids = [row["video_id"] for row in ids]
+
+  return video_ids
